c30999945019bd9d2854b9298eb6ec020f90555b
Glitch (hello-express) <none>
2022-03-17 22:30:40 +0000

    😬🍞 Checkpoint ./public/sketch.js:41447080/691 ./public/index.html:41447080/202

--- public/sketch.js
+++ public/sketch.js
@@ -1,11 +1,3 @@
-// Asking for permision for motion sensors on iOS 13+ devices
-if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-  document.body.addEventListener('click', function () {
-    DeviceOrientationEvent.requestPermission();
-    DeviceMotionEvent.requestPermission();
-  })
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textAlign(CENTER);
@@ -18,19 +10,6 @@ function draw() {
 
   // Go back to corners
   rectMode(CORNER);
-
-  // Calcaulate transparency of left-right
-  // and up-down halves based on tilt of device
-  // RotationXY gives you numbers from -180 to 180.
-  let lr = floor(rotationY);
-  let tb = floor(rotationX);
-  // Ignore flipped over device
-  lr = constrain(lr, -90, 90);
-  tb = constrain(tb, -90, 90);
-
-  // Map rotation to position
-  let x = map(lr, -90, 90, 0, width);
-  let y = map(tb, -90, 90, 0, height);
   
   // Draw ellipse
   ellipse(x, y, 50, 50);
