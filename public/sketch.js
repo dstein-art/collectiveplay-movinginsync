88c894cc5d3b0209732636fd568e7ab7e133a648
Glitch (hello-express) <none>
2022-03-18 22:26:53 +0000

    ✈️🏕 Checkpoint ./public/sketch.js:41447080/390

--- public/sketch.js
+++ public/sketch.js
@@ -1,11 +1,30 @@
+// Open and connect socket
+let socket = io();
+let connected=0;
+
+// Listen for when the socket connects
+socket.on("connect", function () {
+  // Log a success message
+  connected = 1;
+});
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textAlign(CENTER);
 }
 
+function mouseClicked() {
+  socket.emit("data",{x: mouseX, y: mouseY});
+}
+
 function draw() {
   background(255);
-  fill(0);
+  
+  if (connected) {
+    fill(100,100,255);
+  } else {
+    fill(0);
+  }
 
   // Go back to corners
   rectMode(CORNER);
