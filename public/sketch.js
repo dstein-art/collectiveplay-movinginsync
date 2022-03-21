7bc4f55d37e54ddf05c0e2d838a827354f2b7fff
Glitch (hello-express) <none>
2022-03-21 16:57:29 +0000

    🌎🌮 Checkpoint ./public/sketch.js:41447080/2646 ./.glitch-assets:41447080/379 ./public/texture.vert:41447080/987 ./public/texture.frag:41447080/1263 ./public/sketchold.js:41447080/734 ./public/index.html:41447080/115

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,7 @@
+//this variable will hold our shader object
+let simpleShader;
+let count;
+
 // Open and connect socket
 let socket = io();
 let connected=0;
@@ -8,35 +12,54 @@ socket.on("connect", function () {
   connected = 1;
 });
 
+
+function preload(){
+  // a shader is composed of two parts, a vertex shader, and a fragment shader
+  // the vertex shader prepares the vertices and geometry to be drawn
+  // the fragment shader renders the actual pixel colors
+  // loadShader() is asynchronous so it needs to be in preload
+  // loadShader() first takes the filename of a vertex shader, and then a frag shader
+  // these file types are usually .vert and .frag, but you can actually use anything. .glsl is another common one
+  simpleShader = loadShader('texture.vert', 'texture.frag');
+}
+
 function setup() {
-  background(255);
-  createCanvas(windowWidth, windowHeight);
-  textAlign(CENTER);
-  socket.on('data', function(data){
-    rectMode(CORNER);
-    fill(255,100,100);
-    // Draw ellipse
-    ellipse(windowWidth-data.x, windowHeight-data.y, 25, 25);
-    
-  });
+  // shaders require WEBGL mode to work
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  noStroke();
+  cnt=0.0;
 }
 
-function mouseClicked() {
-  socket.emit("data",{x: mouseX, y: mouseY});
+function draw() {  
+  cnt++;
+  
+  let mx = map(mouseX, 0, width, 0.0, 1.0);
+  let my = map(mouseY, 0, height, 0.0, 1.0);
+  
+  // shader() sets the active shader with our shader
+  shader(simpleShader);
+
+  simpleShader.setUniform('mouse1', [mx, my]);
+  simpleShader.setUniform('mouse2', [0.5+0.1*cos(cnt/600), 0.5+0.1*sin(cnt/570)]);
+
+  simpleShader.setUniform('resolution', [width,height]);
+
+  simpleShader.setUniform('point1', [0.0,0.0]);
+  simpleShader.setUniform('point2', [0.35,0.35]);
+  simpleShader.setUniform('point3', [-0.25,0.5]);
+
+  /*
+  simpleShader.setUniform('point1', [sin(2+cnt/1000)+1.0,cos(3+cnt/1000)+1.0]);
+  simpleShader.setUniform('point2', [cos(4+cnt/1333)+1.0,sin(5+cnt/843)+1.0]);
+  simpleShader.setUniform('point3', [sin(6+cnt/897)+1.0,cos(7+cnt/993)+1.0]);
+  */
+
+  
+  // rect gives us some geometry on the screen
+  rect(0,0,width, height);
+
 }
 
-function draw() {
-
-  
-  if (connected) {
-    fill(100,100,255);
-  } else {
-    fill(0);
-  }
-
-  // Go back to corners
-  rectMode(CORNER);
-  
-  // Draw ellipse
-  ellipse(mouseX, mouseY, 50, 50);
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
 }
---
+++ public/texture.frag
@@ -0,0 +1,65 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.14159265358979323846
+
+uniform vec2 resolution;
+uniform float time;
+uniform vec2 mouse1;
+uniform vec2 mouse2;
+
+uniform vec2 point1;
+uniform vec2 point2;
+uniform vec2 point3;
+
+
+void main (void) {
+  vec2 mst1 = mouse1.xy;
+  vec2 mst2 = mouse2.xy;
+
+
+  // Mouse 1
+  vec2 st = 0.5*gl_FragCoord.xy/resolution.xy;
+  st.y=1.0-st.y;
+
+  float dist1 = distance(st.xy,mst1+point1.xy)*4.0;
+  float dist2 = distance(st.xy,mst1+point2.xy)*4.0;
+  float dist3 = distance(st.xy,mst1+point3.xy)*4.0;
+  
+  float color1=dist1*dist2*dist3;
+  color1=floor(color1);
+  
+  if (mod(color1, 2.0)==0.0) {
+    color1=0.0;
+  }
+
+  vec4 myColor= vec4(color1*100.0,color1*100.0,color1*100.0,1.0);
+
+  // Mouse 2
+
+  dist1 = distance(st.xy,mst2+point1.xy)*4.0;
+  dist2 = distance(st.xy,mst2+point2.xy)*4.0;
+  dist3 = distance(st.xy,mst2+point3.xy)*4.0;
+  
+  float color2=dist1*dist2*dist3;
+  color2=floor(color2);
+  
+  if (mod(color2, 2.0)==0.0) {
+    color2=0.2;
+  }
+  
+  if (color2<color1) {
+    myColor=vec4(color2,color2,color2,1.0);   
+  }
+  
+  if (distance(mst1,st)<0.025) {
+    myColor=vec4(1.0,0.0,0.0,0.4);
+  } 
+  
+  if ((abs(st.x)<0.025) || (abs(st.x)>0.975)) {
+    myColor=vec4(0.0,1.0,1.0,1.0);
+  }
+  
+  gl_FragColor = myColor;
+}
